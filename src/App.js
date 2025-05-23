bf4190338162ef4229c519f10b2bc4682103dac6
Choseolmin <daegok1000@gmail.com>
2025-05-23 17:06:40 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
 import { dummyTweets } from './static/dummyData';
+
 // ! 위 코드는 수정하지 않습니다.
 console.log(dummyTweets); // 개발 단계에서 사용하는 더미 데이터입니다.
 
 const Sidebar = () => {
   return (
     <section className="sidebar">
-      {/* TODO : 메세지 아이콘을 작성합니다. */}
+     <i class="far fa-comment-dots"></i>
+  
     </section>
   );
 };
 
-const Counter = () => {
+const Counter = ({total}) => {
   return (
     <div className="tweetForm__input">
       <div className="tweetForm__inputWrapper">
         <div className="tweetForm__count" role="status">
-          TODO : dummyTweet로 전달되는 데이터의 갯수를 보여줍니다.
+        total: {total}
         </div>
       </div>
     </div>
@@ -25,7 +27,11 @@ const Counter = () => {
 };
 
 const Footer = () => {
-  return <div></div>;
+  return(    
+  <footer>
+    ⓒ 2025 My Twitter Clone
+  </footer>
+);
 };
 // TODO : Footer 함수 컴포넌트를 작성합니다. 시멘틱 엘리먼트 footer가 포함되어야 합니다.
 
@@ -35,16 +41,29 @@ const Tweets = () => {
       {dummyTweets.map((tweet) => {
         return (
           <li className="tweet" key={tweet.id}>
-            <div className="tweet__profile">
-              {/* TODO: 트윗 저자의 프로필 사진이 있어야 합니다.  */}
+               <div className="tweet__profile">
+             
+              <img src={tweet.picture} alt={`${tweet.username}의 프로필`} />
             </div>
-            <div className="tweet__content">
+
+              <div className="tweet__content">
               <div className="tweet__userInfo">
-                {/* TODO : 유져 이름이 있어야 합니다. */}
-                {/* TODO : 이름이 "Bob"인 경우, 이름 배경색을 rgb(235, 229, 249)으로 바꿔야 합니다. */}
-                {/* TODO : 트윗 생성 일자가 있어야 합니다. */}
+             
+                <span
+                className={
+                tweet.username === "Bob"
+                ? "tweet__name tweet__username--purple"
+                : "tweet__username"
+                }
+>
+                
+                  {tweet.username}
+                </span>
+
+                <span className="tweet__createdAt">{tweet.createdAt}</span>
               </div>
-              TODO : 트윗 메세지가 있어야 합니다.
+
+              <div className="tweet__message">{tweet.content}</div>
             </div>
           </li>
         );
@@ -63,7 +82,7 @@ const Features = () => {
         </div>
       </div>
       <Tweets />
-      TODO : Footer 컴포넌트를 작성합니다.
+      <Footer/>
     </section>
   );
 };
@@ -72,7 +91,9 @@ const App = () => {
   return (
     <div className="App">
       <main>
-        TODO : Sidebar 컴포넌트를 작성합니다.
+        <Sidebar />
+        <Counter total={dummyTweets.length} />
+
         <Features />
       </main>
     </div>
